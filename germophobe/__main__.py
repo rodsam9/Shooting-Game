05d794561862ce0714d4b87079befc20b01fb90d
astoncook <astoncook2001@gmail.com>
2021-06-26 13:09:31 -0600

    saturday alpha part 1

--- germophobe/__main__.py
+++ germophobe/__main__.py
@@ -1,1 +1,17 @@
 # program entry point
+from assets.director import Director
+from assets import constants
+from assets.actor import Actor
+import random
+import arcade
+import math
+import os
+
+def main():
+    director = Director()
+    director.start_game()
+    arcade.run()
+
+
+if __name__ == "__main__":
+    main()
---
+++ germophobe/assets/constants.py
@@ -0,0 +1,17 @@
+import random
+import arcade
+import math
+import os
+
+SPRITE_SCALING_PLAYER = 0.5
+SPRITE_SCALING_COIN = 0.4
+SPRITE_SCALING_LASER = 0.8
+ENEMY_COUNT = 30
+
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+SCREEN_TITLE = "Germaphobe Alpha"
+
+BULLET_SPEED = 5
+
+window = None
