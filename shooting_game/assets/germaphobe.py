49686be63c59c9c1bbf195209c9a6a8424317535
Sam <sr_odriguez9@yahoo.com>
2021-07-07 15:33:25 -0600

    attempted to fix health

--- shooting_game/assets/germaphobe.py
+++ shooting_game/assets/germaphobe.py
@@ -74,6 +74,45 @@ class ENEMY(arcade.Sprite):
                                      height=HEALTHBAR_HEIGHT,
                                      color=arcade.color.GREEN)
 
+class PLAYER(arcade.Sprite):
+
+    def __init__(self, image, scale, max_health):
+        super().__init__(image, scale)
+
+        # Add extra attributes for health
+        self.max_health = max_health
+        self.cur_health = max_health
+
+    def draw_health_number(self):
+        # Draw how many health the enemies have
+
+        health_string = f"{self.cur_health}/{self.max_health}"
+        arcade.draw_text(health_string,
+                         start_x=self.center_x + HEALTH_NUMBER_OFFSET_X,
+                         start_y=self.center_y + HEALTH_NUMBER_OFFSET_Y,
+                         font_size=12,
+                         color=arcade.color.WHITE)
+
+    def draw_health_bar(self):
+        # Draw the health bar
+
+        # Draw the red background
+        if self.cur_health < self.max_health:
+            arcade.draw_rectangle_filled(center_x=self.center_x,
+                                         center_y=self.center_y + HEALTHBAR_OFFSET_Y,
+                                         width=HEALTHBAR_WIDTH,
+                                         height=3,
+                                         color=arcade.color.RED)
+
+        # Calculate width based on health
+        health_width = HEALTHBAR_WIDTH * (self.cur_health / self.max_health)
+
+        arcade.draw_rectangle_filled(center_x=self.center_x - 0.5 * (HEALTHBAR_WIDTH - health_width),
+                                     center_y=self.center_y - 10,
+                                     width=health_width,
+                                     height=HEALTHBAR_HEIGHT,
+                                     color=arcade.color.GREEN)
+
 class MyGame(arcade.Window):
     """ Main application class. """
 
@@ -103,11 +142,11 @@ class MyGame(arcade.Window):
         self.player_list = arcade.SpriteList()
         self.enemy_list = arcade.SpriteList()
         self.bullet_list = arcade.SpriteList()
-        self.player_sprite = arcade.Sprite(":resources:images/animated_characters/male_adventurer/maleAdventurer_idle.png", SPRITE_SCALING_PLAYER)
+        self.player_sprite = PLAYER(":resources:images/animated_characters/male_adventurer/maleAdventurer_idle.png", SPRITE_SCALING_PLAYER, max_health=10)
         self.player_sprite.center_x = 400
         self.player_sprite.center_y = 300
         self.player_list.append(self.player_sprite)
-
+        
         # Create the enemies
         for i in range(ENEMY_COUNT):
 
